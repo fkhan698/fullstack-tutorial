9c6815323d422c2fb03431c92b032ac796e36235
fkhan698 <fkhan698@hotmail.com>
2019-12-17 17:30:19 -0600

    Books Create/Index/New

--- routes/authors.js
+++ routes/authors.js
@@ -4,40 +4,41 @@ const Author = require('../models/authors')
 
 // All Authors Route
 router.get('/', async (req, res) => {
-    let searchOptions = {}
-    if(req.query.name != null && req.query.name !== ''){
-        searchOptions.name = new RegExp(req.query.name, 'i')
-    }
-    try {
-      const authors = await Author.find(searchOptions)
-      res.render('authors/index', {
-        authors: authors,
-        searchOptions: req.query
-      })
-    } catch (e) {
-      res.redirect('/')
-    }
-  })
+  let searchOptions = {}
+  if (req.query.name != null && req.query.name !== '') {
+    searchOptions.name = new RegExp(req.query.name, 'i')
+  }
+  try {
+    const authors = await Author.find(searchOptions)
+    res.render('authors/index', {
+      authors: authors,
+      searchOptions: req.query
+    })
+  } catch (e){
+    res.redirect('/')
+  }
+})
 
 // New Author Route
-router.get('/new', async (req, res) => {
-    res.render('authors/new', {author: new Author() })
+router.get('/new', (req, res) => {
+  res.render('authors/new', { author: new Author() })
 })
 
 // Create Author Route
-router.post('/',async (req, res) => {
-    const author = new Author({
-        name: req.body.name
+router.post('/', async (req, res) => {
+  const author = new Author({
+    name: req.body.name
+  })
+  try {
+    const newAuthor = await author.save()
+    // res.redirect(`authors/${newAuthor.id}`)
+    res.redirect(`authors`)
+  } catch (e){
+    res.render('authors/new', {
+      author: author,
+      errorMessage: 'Error creating Author'
     })
-    try {
-        const newAuthor = await author.save()
-        res.redirect('authors')
-    } catch (e) {
-        res.render('authors/new', {
-            author: author,
-            errorMessage: 'Error creating author'
-        })
-    }
+  }
 })
 
 module.exports = router
--- server.js
+++ server.js
@@ -10,6 +10,7 @@ if (process.env.NODE_ENV !== 'production') {
   
   const indexRouter = require('./routes/index')
   const authorRouter = require('./routes/authors')
+  const bookRouter = require('./routes/books')
   
   app.set('view engine', 'ejs')
   app.set('views', __dirname + '/views')
@@ -19,8 +20,7 @@ if (process.env.NODE_ENV !== 'production') {
   app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
   
   const mongoose = require('mongoose')
-  mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true,
-    useNewUrlParser: true,})
+  mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true,})
   const db = mongoose.connection
   db.on('error', error => console.error(error))
   db.once('open', () => console.log('Connected to Mongoose'))
@@ -28,5 +28,6 @@ if (process.env.NODE_ENV !== 'production') {
   
   app.use('/', indexRouter)
   app.use('/authors', authorRouter)
+  app.use('/books', bookRouter)
   
   app.listen(process.env.PORT || 3000)
